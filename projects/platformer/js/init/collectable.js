9307f3fe089dcba9fb8e7d5182d3ca00bf0cdbc3
Ubuntu <ubuntu@ip-172-31-61-32.ec2.internal>
2020-06-15 20:37:23 +0000

    basic website

---
+++ projects/platformer/js/init/collectable.js
@@ -0,0 +1,51 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let collectable = window.opspark.collectable;
+
+    let type = {
+        db: {assetKey: 'db', points: 10},
+        max: {assetKey: 'max', points: 20},
+        steve: {assetKey: 'steve', points: 30},
+        grace: {assetKey: 'grace', points: 40},
+        kennedi: {assetKey: 'kennedi', points: 50}
+    };
+    
+    /**
+     * init: Initialize all collectables.
+     * 
+     * GOAL: Add as many collectables as necessary to make your level challenging.
+     * 
+     * Use the createCollectable() Function to create collectables for the level.
+     * See the type Object, above, for the types of collectables and their point values.
+     * 
+     * createCollectable() takes these arguments:
+     *      
+     *      createCollectable(type, x, y, gravity, bounce);
+     * 
+     *      type: The type of the collectable, use the type Object above.
+     *      x: The x coordineate for the collectable.
+     *      y: The y coordineate for the collectable.
+     *      gravity: OPTIONAL The gravitational pull on the collectable.
+     *      bounce: OPTIONAL A factor effecting how much the collectable will bounce off platforms, etc.
+     */ 
+    function init(game) {
+        let createCollectable = collectable.create;
+
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        createCollectable(type.kennedi, 800, 170, 25, 0.9);
+        createCollectable(type.steve, 500, 300, 25, 0.9);
+        createCollectable(type.grace, 210, 100, 25, 0.8);
+        createCollectable(type.db, 432, 45, 5, 1);
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+    collectable.init = init;
+})(window);
